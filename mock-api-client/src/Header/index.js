834977782ba5a5fe0bd1c4dbbac39884de776476
honganh-lt <anhle7534@gmail.com>
2024-12-19 11:29:37 +0700

    upload

--- mock-api-client/src/Header/index.js
+++ mock-api-client/src/Header/index.js
@@ -10,8 +10,7 @@ return (
     <img 
       alt="FITA logo"
       // height={90}
-      src="https://s3-alpha-sig.figma.com/img/d0bb/9c46/8b56d561495fc6c4bb192636d840b2e3?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=aMBsRL~oIfxkTOyWayiEwaw5o5C19JHb-VnuNxRPO99dc5L4fLwJs6MJxAUuArFATviCTGfwGHsVolDisiI3tlgLPCsJs8NW0HKTiaD3Z6AX64~KaRW-MtX9ADk3yAKL96w56tgtaWg5eK4rDV7g1sa69un6TdKYRtKtknsi1YkRN3OkzrWICjHfZ6wAfT03NP6sWtCEQJk8rLH9ZBL2zf-QAeJb5bhloRqYgPs-2I6fwV41dxjPKqtNq5-AySSyYrw1ErHX5yeXZQRmR4aYSEDzAwq4V3svWqZA4PRCVy7TpWsJdF1VsoLJIl-lEAd~EiWTafEccoFMZLSE3CHrFg__"
-      // width={90}
+      src='logoschool.png'      // width={90}
     />
     <div>
         <div className='text-header'>
@@ -33,8 +32,7 @@ return (
       <img
         alt=""
         // height={60}
-        src="https://s3-alpha-sig.figma.com/img/d8be/43b0/7a7e657b44865f3ea528bfe615947a91?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=qFsE0dZRRTqIsr0-jOrDGSddRW8UDufYn0UhbapJeRrGgqfakajBrq6F4PkTX7thQnzsPJuyUERI~G4FjU~XhPngaao4fjGnbd47uujKsZrQELuNZHzvblhDTklkfkr2D3bgkd84AejoWi1yP3xtBj~P7AiQL83UstqIBISpXosea-l8tj-i~zWpqypXVSnITIBFlDSn~4kVFWiEaCJyo2jaegk7SZwO30Rhk~iTQGWv9jDK21NCFJ4GJc7aGubJ7Ee76qo1wOhzeFNP2roGP2~dmBMSKCLZzBt9ucidsFnZ3jp1Lbvnr2jmtAR9Cd8nnyLGqcwxh6RtvYJGp~oQhw__"
-        // width={60}
+        src='logoschool.png'        // width={60}
       />
     </div>
     <div className="auth-links">
--- mock-api-client/src/component/MockUsers.js
+++ mock-api-client/src/component/MockUsers.js
@@ -56,7 +56,9 @@ export default function MockUsers() {
       <header className="header">
         <img
           alt="FITA logo"
-          src="https://s3-alpha-sig.figma.com/img/d0bb/9c46/8b56d561495fc6c4bb192636d840b2e3?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=aMBsRL~oIfxkTOyWayiEwaw5o5C19JHb-VnuNxRPO99dc5L4fLwJs6MJxAUuArFATviCTGfwGHsVolDisiI3tlgLPCsJs8NW0HKTiaD3Z6AX64~KaRW-MtX9ADk3yAKL96w56tgtaWg5eK4rDV7g1sa69un6TdKYRtKtknsi1YkRN3OkzrWICjHfZ6wAfT03NP6sWtCEQJk8rLH9ZBL2zf-QAeJb5bhloRqYgPs-2I6fwV41dxjPKqtNq5-AySSyYrw1ErHX5yeXZQRmR4aYSEDzAwq4V3svWqZA4PRCVy7TpWsJdF1VsoLJIl-lEAd~EiWTafEccoFMZLSE3CHrFg__"
+          // src="https://s3-alpha-sig.figma.com/img/d0bb/9c46/8b56d561495fc6c4bb192636d840b2e3?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=aMBsRL~oIfxkTOyWayiEwaw5o5C19JHb-VnuNxRPO99dc5L4fLwJs6MJxAUuArFATviCTGfwGHsVolDisiI3tlgLPCsJs8NW0HKTiaD3Z6AX64~KaRW-MtX9ADk3yAKL96w56tgtaWg5eK4rDV7g1sa69un6TdKYRtKtknsi1YkRN3OkzrWICjHfZ6wAfT03NP6sWtCEQJk8rLH9ZBL2zf-QAeJb5bhloRqYgPs-2I6fwV41dxjPKqtNq5-AySSyYrw1ErHX5yeXZQRmR4aYSEDzAwq4V3svWqZA4PRCVy7TpWsJdF1VsoLJIl-lEAd~EiWTafEccoFMZLSE3CHrFg__"
+          src='logoschool.png'
+
           />
         <div>
           <div className="search-bar">
@@ -74,8 +76,7 @@ export default function MockUsers() {
       <img
         alt=""
         // height={60}
-        src="https://s3-alpha-sig.figma.com/img/d8be/43b0/7a7e657b44865f3ea528bfe615947a91?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=qFsE0dZRRTqIsr0-jOrDGSddRW8UDufYn0UhbapJeRrGgqfakajBrq6F4PkTX7thQnzsPJuyUERI~G4FjU~XhPngaao4fjGnbd47uujKsZrQELuNZHzvblhDTklkfkr2D3bgkd84AejoWi1yP3xtBj~P7AiQL83UstqIBISpXosea-l8tj-i~zWpqypXVSnITIBFlDSn~4kVFWiEaCJyo2jaegk7SZwO30Rhk~iTQGWv9jDK21NCFJ4GJc7aGubJ7Ee76qo1wOhzeFNP2roGP2~dmBMSKCLZzBt9ucidsFnZ3jp1Lbvnr2jmtAR9Cd8nnyLGqcwxh6RtvYJGp~oQhw__"
-        // width={60}
+        src='hat.png'        // width={60}
       />
     </div>
         <div className="auth-links">
@@ -100,7 +101,7 @@ export default function MockUsers() {
   <img
     alt="Illustration of a person studying online with a computer"
     height={400}
-    src="https://s3-alpha-sig.figma.com/img/a15a/4d08/b2549db74d0736a6c6038f2c86f73832?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=fDdSmGt-4-O8PCfDffxF8XBydPX8~dkhFsXkc6J2~m47YeSFo-YnC8CLT9rp62dsHLPEOl8ul~srf0mnaUkjI8tAgE-ocOYj7d-pAcRq5cS-8UfXDyZt~-qk0QhEQZuagabRcL6DCmst5RQ16Z5rX5M3QC81ZygXU6zrltZIe4Pu4~HYhpK86qWoWCYlA1O~sUgWnVSNUg6yPGE5dwZLMasFZszpoYrG7fCDZen01Psg-8I91mDPwam3rcTsw5pVtLdMhN1XZdM8UdDSLuGVy-4QlDOGf2BWvf~iLXi7pxnD5lK-EL0GerN6TXiRREcaUUPM8gvuVK4~OGKoqUGrtg__"
+    src='banner.jpg'    
     width={800}
   />
 </div>
